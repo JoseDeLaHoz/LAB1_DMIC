3d45b6438242976300e09da9e89a9a2c8adf6dd7
Jose De La Hoz <jose.hoz@mail.escuelaing.edu.co>
2022-09-28 12:58:47 -0500

    main/Add Interface

--- workspace/Uart/Core/Src/main.c
+++ workspace/Uart/Core/Src/main.c
@@ -107,15 +107,20 @@ int main(void)
   printf("\x1b[2J"); //Clear the screen
   printf("\n\r***************************************************************");
   printf("\n\r* Universidad Escuela Colombiana de Ingenieria Julio Garavito *");
-  printf("\n\r* DMIC - Jose De La Hoz - 2022                                *");
-  printf("\n\r* Juego 'GENERADOR SEUDO SBPA'                          *");
+  printf("\n\r*                    DMIC - B - 2022                          *");
+  printf("\n\r*\t Jose De La Hoz                                      *");
+  printf("\n\r*\t Jhan Carlos Reyes                                   *");
+  printf("\n\r*\t Esneider Silva                                      *");
+  printf("\n\r*\t\t'GENERADOR SEUDO SBPA'                          *");
   printf("\n\r* Desarrollado para una tarjeta NUCLE0-F767ZI                 *");
   printf("\n\r***************************************************************");
   printf("\r\n");
   printf("\n\rPara una mejor experiencia, habilite la opcion de eco local en su terminal");
   printf("\r\n");
-  printf("\r\nIngrese su nombre: ");
-  //scanf("%s", nombre);
+  //printf("\r\nIngrese su nombre: ");
+  printf("\n\rNumero de bits termino de realimentacion");
+  scanf("%s", buf);
+  int numUsuario = atoi(buf);
 
 
   /* USER CODE END 2 */
